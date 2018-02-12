990d7fa835eec3c3f24b5a898b778d57882e35fb
Thiago-Medeiros <dev.thiagomedeiros@gmail.com>
2018-02-12 13:10:25 -0200

    refactor: download.sh script

--- docker/php-fpm/script/download.sh
+++ docker/php-fpm/script/download.sh
@@ -1,4 +1,7 @@
+#!/usr/bin/env bash
 : ${MAGENTO_VERSION:="2.1.10"}
-curl -sL https://github.com/magento/magento2/archive/$MAGENTO_VERSION.tar.gz | tar -zxvf - -C /tmp/ && \
-    mv /tmp/magento2-$MAGENTO_VERSION/.[!.]* /var/www && \
-    rm -rf /tmp/magento2-$MAGENTO_VERSION
+shopt -s dotglob
+curl -sL https://github.com/magento/magento2/archive/${MAGENTO_VERSION}.tar.gz | tar -zxvf - -C /tmp/
+mv /tmp/magento2-${MAGENTO_VERSION}/* /var/www
+rm -rf /tmp/magento2-${MAGENTO_VERSION}
+shopt -u dotglob
