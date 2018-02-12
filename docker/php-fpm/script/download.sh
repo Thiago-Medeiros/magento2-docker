834466f96cfca387f9591fd29e7dce96c82e0295
Thiago-Medeiros <dev.thiagomedeiros@gmail.com>
2018-02-12 12:04:37 -0200

    update: .gitattributes

--- docker/php-fpm/script/download.sh
+++ docker/php-fpm/script/download.sh
@@ -1,5 +1,4 @@
-MAGENTO=$1
-: ${MAGENTO:="2.1.10"}
-curl -sL https://github.com/magento/magento2/archive/$MAGENTO.tar.gz | tar -zxvf - -C /tmp/ && \
-    mv /tmp/magento2-$MAGENTO/.[!.] /var/www && \
-    rm -rf /tmp/magento2-$MAGENTO
+: ${MAGENTO_VERSION:="2.1.10"}
+curl -sL https://github.com/magento/magento2/archive/$MAGENTO_VERSION.tar.gz | tar -zxvf - -C /tmp/ && \
+    mv /tmp/magento2-$MAGENTO_VERSION/.[!.]* /var/www && \
+    rm -rf /tmp/magento2-$MAGENTO_VERSION
