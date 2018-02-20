4e27215fe9f9a0b7bc84515d74f0112715ab4c39
Thiago Medeiros | S2IT <thiago.medeiros@s2it.com.br>
2018-02-20 09:37:40 -0300

    update: CRLF to LF

--- docker/php-fpm/script/download.sh
+++ docker/php-fpm/script/download.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-: ${MAGENTO_VERSION:="2.1.10"}
 shopt -s dotglob
 curl -sL https://github.com/magento/magento2/archive/${MAGENTO_VERSION}.tar.gz | tar -zxvf - -C /tmp/
 mv /tmp/magento2-${MAGENTO_VERSION}/* /var/www
