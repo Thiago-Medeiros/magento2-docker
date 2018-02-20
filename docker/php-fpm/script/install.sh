efecd5d2f21b22d8d14cb9febde82d279f28d6f9
Thiago-Medeiros <dev.thiagomedeiros@gmail.com>
2018-02-20 16:38:11 -0300

    refactor: change db image

--- docker/php-fpm/script/install.sh
+++ docker/php-fpm/script/install.sh
@@ -1,5 +1,6 @@
 php bin/magento setup:install --base-url=http://localhost/ \
-    --db-host=mariadb --db-name=default --db-user=root --db-password=root \
+    --db-host=database --db-name=default --db-user=root --db-password=root \
     --admin-firstname=Magento --admin-lastname=Admin --admin-email=user@example.com \
     --admin-user=admin --admin-password=admin123 --language=pt_BR --session-save=db \
     --currency=BRL --timezone=America/Sao_Paulo --use-rewrites=1 --backend-frontname=admin
+php bin/magento deploy:mode:set developer
