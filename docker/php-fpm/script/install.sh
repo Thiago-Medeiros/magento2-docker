21ab5d3a393cb7434d0602d5a0f34f6a1625fb45
Thiago-Medeiros <dev.thiagomedeiros@gmail.com>
2018-03-15 08:06:06 -0300

    refactor: magento installer script

--- docker/php-fpm/script/install.sh
+++ docker/php-fpm/script/install.sh
@@ -4,11 +4,18 @@ mv /tmp/magento2-${MAGENTO_VERSION}/* /var/www
 rm -rf /tmp/magento2-${MAGENTO_VERSION}
 shopt -u dotglob
 
-composer install -d=/var/www
+wait
+
+composer install -d=/var/www &&
+
+wait
 
 php bin/magento setup:install --base-url=http://localhost/ \
     --db-host=database --db-name=default --db-user=root --db-password=root \
     --admin-firstname=Magento --admin-lastname=Admin --admin-email=user@example.com \
     --admin-user=admin --admin-password=admin123 --language=pt_BR --session-save=db \
-    --currency=BRL --timezone=America/Sao_Paulo --use-rewrites=1 --backend-frontname=admin
+    --currency=BRL --timezone=America/Sao_Paulo --use-rewrites=1 --backend-frontname=admin &&
+
+wait
+
 php bin/magento deploy:mode:set developer
